523ba47d2961387e710d068ae56d20b3d5474008
HuangZhou <zhmail0830@gmail.com>
2025-02-13 18:36:56 +0800

    feat: :sparkles: integrated shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Button } from '@/components/ui/button';
+
 const App: React.FC = () => {
-  return <h1>Welcome to customAIze!</h1>;
+  return (
+    <>
+      <h1>Welcome to customAIze!</h1>
+      <Button>Click me</Button>
+    </>
+  );
 };
 
 export default App;
